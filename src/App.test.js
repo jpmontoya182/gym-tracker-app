c16ac9da61271534f960635c982e15555be161c7
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2019-05-05 22:43:35 -0500

    it was added the configurations to jest and coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) {
+    wrapper.setState(state);
+  }
+  return wrapper;
+};
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
+// SuccessMessage
+test('Component render withot errors', () => {
+  const wrapper = setup();
+  const headerComponent = findByTestAttr(wrapper, 'AppComponent');
+  expect(headerComponent.length).toBe(1);
 });
